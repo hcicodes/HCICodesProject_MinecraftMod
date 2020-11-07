f84e8692ce619190e4ce7e00471e9e2a7b771eaa
c-ola <59809814+c-ola@users.noreply.github.com>
2020-11-07 15:47:43 -0500

    Added more items and blocks
    Added sourcite, classite, methodite, jarium. Blocks for sourcite ore and jarium block. No textures yet.

--- src/main/java/com/hci/humbercraft/tools/RegistryHandler.java
+++ src/main/java/com/hci/humbercraft/tools/RegistryHandler.java
@@ -32,7 +32,20 @@ public class RegistryHandler {
 	public static final RegistryObject<Item> JAVIUM_BLOCK_ITEM = ITEMS.register("javium_block", () -> new BlockItemBase(JAVIUM_BLOCK.get()));
 	
 	public static final RegistryObject<Block> JAVIUM_ORE_BLOCK = BLOCKS.register("javium_ore_block", JaviumOreBlock::new);	
-	public static final RegistryObject<Item> JAVIUM_ORE_BLOCK_ITEM = ITEMS.register("javium_ore_block", () -> new BlockItemBase(JAVIUM_ORE_BLOCK.get()));
+	public static final RegistryObject<Item> JAVIUM_ORE_BLOCK_ITEM = ITEMS.register("javium_ore_block", () -> new BlockItemBase(JAVIUM_ORE_BLOCK.get()));	
 	
+	public static final RegistryObject<Block> SOURCITE_ORE_BLOCK = BLOCKS.register("sourcite_ore_block", () -> new BlockBase(Block.Properties.create(Material.IRON).hardnessAndResistance(5.0f, 5.0f).sound(SoundType.STONE).harvestLevel(2).harvestTool(ToolType.PICKAXE)));	
+	public static final RegistryObject<Item> SOURCITE_ORE_BLOCK_ITEM = ITEMS.register("sourcite_ore_block", () -> new BlockItemBase(SOURCITE_ORE_BLOCK.get()));
+	
+	public static final RegistryObject<Item> SOURCITE = ITEMS.register("sourcite", ItemBase::new);	
+	public static final RegistryObject<Item> CLASSITE = ITEMS.register("classite", ItemBase::new);
+	public static final RegistryObject<Item> METHODITE = ITEMS.register("methodite", ItemBase::new);
+	
+	public static final RegistryObject<Item> JARIUM = ITEMS.register("jarium", ItemBase::new);
+	public static final RegistryObject<Block> JARIUM_BLOCK = BLOCKS.register("jarium_block", () -> new BlockBase(Block.Properties.create(Material.IRON).hardnessAndResistance(5.0f, 5.0f).sound(SoundType.STONE).harvestLevel(2).harvestTool(ToolType.PICKAXE)));		
+	public static final RegistryObject<Item> JARIUM_BLOCK_ITEM = ITEMS.register("jarium_block", () -> new BlockItemBase(JARIUM_BLOCK.get()));
+	
+	
+
 	
 }
